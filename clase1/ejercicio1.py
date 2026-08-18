0c1a295772df0f5bb116450f2852bd5e82cef4b5
Ruben Nahuel Campo Ledesma <96516674+NahuCampo@users.noreply.github.com>
2026-08-18 20:28:32 +0000

    primeros 2 ejercicios

---
+++ clase1/ejercicio1.py
@@ -0,0 +1,2 @@
+nombre = input("Como te llamas?\n")
+print("hola",nombre,"Mucho gusto")
---
+++ clase1/ejercicio2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+suma = num1 + num2
+print("La suma es:",suma)
+print("la resta")
+print("multiplicacion")
+print("Division")
