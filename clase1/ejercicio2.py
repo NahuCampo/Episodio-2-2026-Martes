bf161afb379a979e910401ff98f40e5c59f43ab2
Ruben Nahuel Campo Ledesma <96516674+NahuCampo@users.noreply.github.com>
2026-08-18 20:33:24 +0000

    agregue resta a ej2

--- clase1/ejercicio2.py
+++ clase1/ejercicio2.py
@@ -1,7 +1,8 @@
 num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 suma = num1 + num2
+resta = num1-num2
 print("La suma es:",suma)
-print("la resta")
+print("la resta es:",resta)
 print("multiplicacion")
 print("Division")
